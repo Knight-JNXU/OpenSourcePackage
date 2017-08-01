a5c1a656b06528cf8e58fc08254cf186ff3b5486
KnightDevelop <knightdevelop@sina.com>
2017-08-01 21:20:37 +0800

    新增 jsoup 的用法

--- src/main/java/usages/JsoupUsageUsage.java
+++ src/main/java/usages/JsoupUsageUsage.java
@@ -1,40 +1,9 @@
-/**
- 
-* Copyright (c) 2014 Baozun All Rights Reserved.
- 
-*
- 
-* This software is the confidential and proprietary information of Baozun.
- 
-* You shall not disclose such Confidential Information and shall use it only in
- 
-* accordance with the terms of the license agreement you entered into
- 
-* with Baozun.
- 
-*
- 
-* BAOZUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
- 
-* SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- 
-* IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- 
-* PURPOSE, OR NON-INFRINGEMENT. BAOZUN SHALL NOT BE LIABLE FOR ANY DAMAGES
- 
-* SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
- 
-* THIS SOFTWARE OR ITS DERIVATIVES.
- 
-*
- 
-*/
 package usages;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -49,41 +18,63 @@ import org.slf4j.LoggerFactory;
  * @date 2017年7月28日
  *       funcation :
  */
-public class JsoupUsageUsage{
+public class JsoupUsageUsage {
 
     private Logger logger = LoggerFactory.getLogger(JsoupUsageUsage.class);
 
-    private String readFileUtil(String filePath){
-        String lineTxt = "";
-        try{
-            String encoding = "UTF-8";
-            File file = new File(filePath);
-            if (file.isFile() && file.exists()){ //判断文件是否存在
-                InputStreamReader read = new InputStreamReader(new FileInputStream(file), encoding);//考虑到编码格式
-                BufferedReader bufferedReader = new BufferedReader(read);
-                while (bufferedReader.readLine() != null){
-                    lineTxt += (bufferedReader.readLine());
-                }
-                read.close();
-            }else{
-                System.out.println("找不到指定的文件");
-            }
-        }catch (Exception e){
-            System.out.println("读取文件内容出错");
-            e.printStackTrace();
-        }
-        return lineTxt;
+    private String readFileJava7(String filePath) throws Exception{
+        return new String(Files.readAllBytes(Paths.get(filePath)));
+    }
+    private String readFileJava8(String filePath) throws Exception{
+        List<String> lines = Files.readAllLines(Paths.get(filePath), StandardCharsets.UTF_8);
+        return String.join("\n", lines);
     }
 
-    @Test
+
     /**
-     * 通过class值获取html值
+     * 通过class值获取html
      */
+    @Test
     public void getInnerHtmlByClassName() throws Exception{
-        String fileStr = readFileUtil("D:/htmlContent.txt");
+        String fileStr = readFileJava7("D:/htmlContent.txt");
         Document document = Jsoup.parse(fileStr, "UTF-8");
-        Elements element = document.select(".description-text");
-        logger.info(element.toString());
+        Elements elements = document.select(".description-text");
+        logger.info(elements.toString());
+
+        logger.info("---------------------------------");
+
+        elements = document.getElementsByClass(".description-text");
+        logger.info(elements.toString());
+    }
+
+    /**
+     * 通过 class 和 标签 获取html
+     * @throws Exception
+     */
+    @Test
+    public void getInnerHtmlByClassNameAngTagName() throws Exception{
+        String fileStr = readFileJava8("D:/htmlContent.txt");
+        Document document = Jsoup.parse(fileStr, "UTF-8");
+        Elements elements = document.select(".description-text h2");
+        logger.info(elements.toString());
+    }
+
+    /**
+     * 通过 class 和 标签 获取html值
+     * @throws Exception
+     */
+    @Test
+    public void getInnerHtmlValueByClassNameAngTagName() throws Exception{
+        String fileStr = readFileJava8("D:/htmlContent.txt");
+        Document document = Jsoup.parse(fileStr, "UTF-8");
+        Elements elements = document.select(".description-text h2");
+        logger.info(elements.text());
+
+        elements = document.select(".text-a p");
+        logger.info(elements.text());
+
+        elements = document.select(".text-a span");
+        logger.info(elements.text());
     }
 
 }
